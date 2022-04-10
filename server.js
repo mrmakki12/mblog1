27ebf342149ac91979ee1d34bc1da671ccd6608f
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-10 17:41:42 -0400

    port

--- server.js
+++ server.js
@@ -104,6 +104,6 @@ app.post('/api/v1/articles/:id/comments', async (req, res, next) => {
 // listen
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
-    console.log(`Server now listening on PORT ${port}`);
+app.listen(5432, () => {
+    console.log(`Server now listening on PORT 5432`);
 });
