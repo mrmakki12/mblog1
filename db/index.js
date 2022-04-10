4d0b2df128d3b871bb709e4699e0511831101da9
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-09 20:13:40 -0400

    think its working now

--- db/index.js
+++ db/index.js
@@ -6,3 +6,8 @@ const pool = new Pool();
 module.exports = {
     query: (text, params) => pool.query(text, params)
 };
+
+// postgres://rfllaxqjoxpkdw:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39@ec2-34-207-12-160.compute-1.amazonaws.com:5432/ddch46khuff2fa
+
+// username: rfllaxqjoxpkdw
+// password:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39 
