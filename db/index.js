cdc54fc7c447d9696e98af671fb5235d500b86d7
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-11 11:43:59 -0400

    k

--- db/index.js
+++ db/index.js
@@ -1,22 +1,12 @@
 // require pg to connect to postgres db
-const { Pool, Client } = require('pg'); 
+const { Pool } = require('pg');
 
-// const pool = new Pool({
-//     user: 'rfllaxqjoxpkdw',
-//     database: 'ddch46khuff2fa',
-//     port: 5432,
-//     host: 'ec2-34-207-12-160.compute-1.amazonaws.com',
-//     password: 'f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39'
-// });
+const pool = new Pool();
 
-
-
-var conString = "postgres://rfllaxqjoxpkdw:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39@ec2-34-207-12-160.compute-1.amazonaws.com:5432/ddch46khuff2fa";
-var client = new Client(conString);
-client.connect();
+console.log(pool)
 
 module.exports = {
-    query: (text, params) => client.query(text, params)
+    query: (text, params) => pool.query(text, params)
 };
 
 // postgres://rfllaxqjoxpkdw:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39@ec2-34-207-12-160.compute-1.amazonaws.com:5432/ddch46khuff2fa
