f75a520b8abc7f6edbeb775dfaf7457289405081
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-11 11:01:06 -0400

    pool config

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
 // require pg to connect to postgres db
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+    user: 'rfllaxqjoxpkdw',
+    database: 'ddch46khuff2fa',
+    port: 5432,
+    host: 'ec2-34-207-12-160.compute-1.amazonaws.com',
+    password: 'f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39'
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
