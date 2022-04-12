98837adef3b403ec96a43f773fe5d09bf8c18aab
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-12 09:16:39 -0400

    clearDB mysql

--- db/index.js
+++ db/index.js
@@ -1,17 +1,8 @@
-// require pg to connect to postgres db
-const { Pool } = require('pg');
+const mysql = require('mysql');
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.CLEARDB_DATABASE_URL;
 
-const pool = new Pool({
-    connectionString
-});
+const db = mysql.createConnection(connectionString);
 
-module.exports = {
-    query: (text, params) => pool.query(text, params)
-};
+module.exports = db;
 
-// postgres://rfllaxqjoxpkdw:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39@ec2-34-207-12-160.compute-1.amazonaws.com:5432/ddch46khuff2fa
-
-// username: rfllaxqjoxpkdw
-// password:f994f0ad163cd7fca5565264067f51a3fddc55d18fcfccab776eed0f38a49a39 
