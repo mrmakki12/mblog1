06a99689a5be2b97f1c7ffb8659e6c51a643d011
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-12 08:26:45 -0400

    connectString

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 // require pg to connect to postgres db
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+    connectionString
+});
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
