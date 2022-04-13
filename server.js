677a2a6ec779247fb5a34bd55116cb619d00977a
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-13 15:17:51 -0400

    update and create article routes

--- server.js
+++ server.js
@@ -68,11 +68,11 @@ app.post('/api/v1/articles/:id/comments', async (req, res, next) => {
         });
 });
 
+// create article
 app.post('/api/v1/articles/create', async (req, res, next) => {
-
     // post article in database
     db.query(`INSERT INTO articles (user_name, created, title, subtitle, mardown) 
-    VALUES (?, ?, ?, ?)`, [req.body.user_name, req.body.created, req.body.title, req.body.subtitle, req.body.mardown], (err, result) => {
+    VALUES (?, ?, ?, ?)`, [req.body.user_name, req.body.created, req.body.title, req.body.subtitle, req.body.markdown], (err, result) => {
         if(err) {
             console.log(err);
         }
@@ -80,11 +80,11 @@ app.post('/api/v1/articles/create', async (req, res, next) => {
     });
 });
 
+// edit article
 app.put('/api/v1/aritcles/:id/edit', async (req, res, next) => {
-
     // update article
     db.query(`UPDATE TABLE articles SET created = ?, title = ?, subtitle = ? mardown = ?
-    WHERE id = ?`, [req.params.id], (err, result) => {
+    WHERE id = ?`, [new Date().toISOString(), req.body.title, req.body.subtitle, req.body.markdown], (err, result) => {
         if(err) {
             console.log(err);
         }
