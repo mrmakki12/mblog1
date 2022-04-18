1688da2c0a457858bbe276c8d6ce1d043a5a2ba0
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-18 16:51:15 -0400

    h

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ app.post('/api/v1/login', (req, res) => {
 
                 // user found
                 req.session.user = user[0].username;
-                req.cookies.user = user[0].username;
                 console.log(req.session);
                 res.status(200).send({message: 'Success' , user})
             }
