9a0e6c964f55a40e8377a2653843fabdff844566
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-04-22 15:44:31 -0400

    p

--- server.js
+++ server.js
@@ -252,8 +252,8 @@ app.put('/api/v1/articles/:id/edit', authenticated, async (req, res, next) => {
  * when someone lands here we send the entry point index.html
  * and react router handles the rest
  **/ 
-app.get('/profile', authenticated, (req, res) => {
-    res.sendFile(__dirname + `./frontend/build/index.html/profile`);
+app.get('*', authenticated, (req, res) => {
+    res.sendFile(__dirname + `/frontend/build/index.html`);
 });
 
 // listen
